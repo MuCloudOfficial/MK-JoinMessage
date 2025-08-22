7a12d9038360c2f79e510710496dd14243200929
Mu_Cloud <Mu_Cloud@outlook.de>
2025-08-22 20:57:31 +0800

    Change Shadow plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,10 @@
 import org.jetbrains.kotlin.gradle.dsl.JvmTarget
-import java.io.FileReader
 
 plugins {
     java
     idea
     kotlin("jvm") version "2.1.20"
-    id("com.github.johnrengelman.shadow") version "8.1.1"
+    id("com.gradleup.shadow") version "9.0.2"
 }
 
 group = "me.mucloud"
@@ -20,7 +19,6 @@ repositories {
 
 dependencies {
     implementation(kotlin("stdlib-jdk8"))
-    testImplementation("junit:junit:4.13.1")
 
     compileOnly("org.spigotmc:spigot-api:1.20.6-R0.1-SNAPSHOT")
     compileOnly("me.clip:placeholderapi:2.11.6")
@@ -32,7 +30,6 @@ java{
 }
 
 tasks{
-
     withType<JavaCompile>{
         options.encoding = "UTF-8"
     }
@@ -41,23 +38,12 @@ tasks{
         compilerOptions.jvmTarget.set(JvmTarget.JVM_11)
     }
 
-    test{
-        useJUnitPlatform()
-    }
-
-    jar{
-        archiveFileName.set("MK-JoinMessage-KotlinEdition_${project.version}.jar")
-    }
-
     shadowJar{
-
         archiveFileName = "${rootProject.name}_$version.$DEV.jar"
-
         dependencies{
             include(dependency("org.jetbrains.kotlin:.*"))
             include(dependency("org.jetbrains.kotlinx:.*"))
         }
         relocate("kotlin", "me.mucloud.mcplugin.MK.JoinMessage.libs.kotlin")
     }
-
 }
